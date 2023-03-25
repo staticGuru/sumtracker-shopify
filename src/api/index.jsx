7d205e07d9b3d1583100e10a1fe95609655f45e8
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-25 23:37:31 +0530

    add get contacts list sections

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,19 +1,31 @@
 import axios from "axios";
 
-export const getProductList = async (page = 0) => {
+export const getProductList = async (page = 0, contact) => {
+  console.log("getProductList====>", page, contact);
+  if (page == 1) {
+    page = 0;
+  }
   const config = {
     headers: { Authorization: `Token ${import.meta.env.VITE_AUTH_TOKEN}` },
   };
   return await axios
-    .get(`${import.meta.env.VITE_BASE_URL}/products/?&limit=25&offset=${page}`, config)
+    .get(
+      `${import.meta.env.VITE_BASE_URL}/products/?&limit=25&offset=${page}${
+        contact ? `&contact=${contact}` : ""
+      }`,
+      config
+    )
     .then((res) => res.data);
 };
 
 export const getContactsList = async (searchText = "") => {
-     const config = {
-       headers: { Authorization: `Token ${import.meta.env.VITE_AUTH_TOKEN}` },
-     };
-     return await axios
-       .get(`${import.meta.env.VITE_BASE_URL}/contacts/?search=${searchText}`, config)
-       .then((res) => res.data);
-   };
+  const config = {
+    headers: { Authorization: `Token ${import.meta.env.VITE_AUTH_TOKEN}` },
+  };
+  return await axios
+    .get(
+      `${import.meta.env.VITE_BASE_URL}/contacts/?search=${searchText}`,
+      config
+    )
+    .then((res) => res.data);
+};
