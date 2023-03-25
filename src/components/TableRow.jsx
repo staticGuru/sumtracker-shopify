75b55687b9e90d7d87e4cae095b37fd2791bf785
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-26 00:40:06 +0530

    add the rows sections

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 
 function TableRow({id,sku,variant_name,name,image_url}) {
-  console.log("props", id,sku,variant_name,name,image_url);
   return (
-    <tr class="bg-white border-4 border-gray-200">
+    <tr class="bg-white border-2 border-gray-200">
       <td class="px-16 py-2 flex flex-row items-center">
         <img
-          class="h-8 w-8  object-cover"
-          src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
+          class="h-12 w-12 object-cover"
+          src={image_url}
           alt=""
         />
       </td>
       <td>
-        <span class="text-center ml-2 font-semibold">{id}</span>
+        <span class="text-center ml-2 font-semibold">{sku}</span>
       </td>
       <td class="px-16 py-2">
-        <span>Product Name</span>
+        <span>{name}</span>
       </td>
       <td class="px-16 py-2">
-        <span>Veriant name</span>
+        <span>{variant_name}</span>
       </td>
     </tr>
   );
