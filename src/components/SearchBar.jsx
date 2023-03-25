bea35002b7fe91c121e37567ed7fbf5fc38f0984
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-26 00:39:37 +0530

    add the search bar sections

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { getContactsList } from "../api";
+import { ContactState } from "../context/ContactProvider";
 import { useDebounce } from "../hooks/useDebounce";
 
 function SearchBar() {
-  const [searchText, setSearchText] = useState("");
+  const { setContact, setPage,contact} = ContactState();
+  const [searchText, setSearchText] = useState(contact||"");
   const debouncedSearch = useDebounce(searchText, 500);
   const [showSuggestions, setShowSuggestions] = useState(false);
-  //   const [suggestions, setSuggestions] = useState(["fs", "sdf", "fsdf", "sdf"]);
+
   const { data: suggestions, isLoading } = useQuery({
     queryKey: ["contacts", debouncedSearch],
     queryFn: async () => await getContactsList(debouncedSearch),
@@ -17,18 +19,14 @@ function SearchBar() {
     const inputText = e.target.value;
     setSearchText(inputText);
     setShowSuggestions(inputText.length > 0);
-    //Here I need to call the api endpoints
-    //     let result = await getContactsList(inputText);
-    //     console.log(result);
   };
   const selectContact = (suggestion) => {
     setSearchText(suggestion.company_name);
     setShowSuggestions(false);
-    console.log("sleecteddd======>");
+    setContact(suggestion.id);
+    setPage(1);
     let searchParams = new URLSearchParams(window.location.search);
-
     searchParams.set("contact", suggestion.id);
-
     if (window.history.replaceState) {
       const url =
         window.location.protocol +
@@ -37,7 +35,6 @@ function SearchBar() {
         window.location.pathname +
         "?" +
         searchParams.toString();
-        console.log("url======>",url);
       window.history.replaceState(
         {
           path: url,
@@ -48,9 +45,17 @@ function SearchBar() {
     }
   };
   return (
-    <div className="relative">
+    <div className="relative mb-3">
       <div class="relative w-1/4">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <input
+          type="text"
+          id="Search"
+          value={searchText}
+          onChange={handleInputChange}
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm font-medium rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5  dark:bg-gray-300 dark:border-gray-600 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search by Name/SKU"
+        />
+        <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
           <svg
             aria-hidden="true"
             class="w-5 h-5 text-gray-500 dark:text-gray-400"
@@ -61,22 +66,14 @@ function SearchBar() {
             <path
               fill-rule="evenodd"
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </div>
-        <input
-          type="text"
-          id="Search by Name/SKU"
-          value={searchText}
-          onChange={handleInputChange}
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search"
-        />
       </div>
 
       {showSuggestions && (
-        <div className="absolute z-10 mt-1 w-1/4 bg-white rounded-md shadow-lg">
+        <div className="absolute z-10 mt-1 w-1/4 bg-white rounded-md shadow-lg max-h-[15rem] overflow-scroll">
           {isLoading
             ? "Loading..."
             : suggestions.results.map((suggestion, index) => (
@@ -85,7 +82,6 @@ function SearchBar() {
                   className="px-4 py-2 text-sm text-gray-900 cursor-pointer hover:bg-gray-100"
                   onClick={() => {
                     selectContact(suggestion);
-                    // Call your search function with the selected suggestion
                   }}
                 >
                   {suggestion.company_name}
