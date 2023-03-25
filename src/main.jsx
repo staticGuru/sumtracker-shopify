ff4480233aacaa715be7a9adbe6dfb346a8e864c
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-25 16:22:41 +0530

    add the tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ContactProvider from "./context/ContactProvider";
 import "./index.css";
-
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ContactProvider>
-      <App />
-    </ContactProvider>
+    <QueryClientProvider client={queryClient}>
+      <ContactProvider>
+        <App />
+      </ContactProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
