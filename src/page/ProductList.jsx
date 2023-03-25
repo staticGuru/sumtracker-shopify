361407fa0c37688bdd338fd8567d4c949a51bbdf
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-26 00:40:49 +0530

    create the UI matching sections

--- src/page/ProductList.jsx
+++ src/page/ProductList.jsx
@@ -8,6 +8,12 @@ function ProductList() {
   if (fetchStatus == "loading") return "Loading...";
   return (
     <div>
+      <span className="flex text-2xl align-left font-bold text-violet-500 mb-1">
+        Product List
+      </span>
+      <span className="flex text-sm align-left font-normal text-slate-700 mb-4">
+        List of all Products created in Sumtracker based on unique SKU
+      </span>
       <Header />
       <ContactTable />
     </div>
